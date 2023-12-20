d79ac5da80c2303509038c970f7fcfa0f2aaa13d
xprss <vincenzo.sagristano@studenti.polito.it>
2023-12-20 17:31:02 +0100

    fix: input validation

--- gameoflife.c
+++ gameoflife.c
@@ -11,14 +11,24 @@ int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        printf("Usage: ./main [time_interval (ms)] [amount_of_bacteria (<=%d)]", WIDTH * HEIGHT);
+        printf("Usage: ./main [time_interval (ms)] [amount_of_bacteria (<=%d)]\n", MAX_BACTERIAS);
         return -1;
     }
+
     int time_interval = atoi(argv[1]);
     int amount_of_bacteria = atoi(argv[2]);
-
     field_t field = field_init();
     new_bacteria_n(&field, amount_of_bacteria);
+
+    if(time_interval < 0) {
+        printf("The time interval must be a positive number.\n");
+        return -2;
+    }
+
+    if(amount_of_bacteria > MAX_BACTERIAS) {
+        printf("The amout of bacterias must be less or equal than %d.\n", MAX_BACTERIAS);
+        return -3;
+    }
 
     while (field.current_generation < GENERATIONS)
     {
